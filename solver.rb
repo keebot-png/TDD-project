beefa9103c559896cfb64a1fd25e0fafda86f616
yonas44 <tesfu.yonas1350@gmail.com>
2023-02-10 11:56:51 +0300

    Follow TDD to create fizzbuzz method for Solver class

--- solver.rb
+++ solver.rb
@@ -1,12 +1,29 @@
 class Solver
-    def factorial(n)
-        return 1 if n === 1
-        n * factorial(n - 1)
-    end
-    
-    def reverse(str)
-        str.reverse
-    end
+  def factorial(num)
+    return 1 if num == 1
 
-    
+    n * factorial(num - 1)
+  end
+
+  def reverse(str)
+    str.reverse
+  end
+
+  def fizzbuzz(num)
+    str = '1'
+    i = 2
+    while i <= num
+      str += if (i % 15).zero?
+               ', fizzbuzz'
+             elsif (i % 3).zero?
+               ', fizz'
+             elsif (i % 5).zero?
+               ', buzz'
+             else
+               ", #{i}"
+             end
+      i += 1
+    end
+    str
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,19 +2,25 @@ require './solver'
 require 'rspec'
 
 describe Solver do
-    before :each do
-        @solver = Solver.new
-    end
+  before :each do
+    @solver = Solver.new
+  end
 
-    describe "#factorial" do
-        it "successfully implements the mathematical factorial method" do
-            expect(@solver.factorial(5)).to eq 120
-        end
+  describe '#factorial' do
+    it 'successfully implements the mathematical factorial method' do
+      expect(@solver.factorial(5)).to eq 120
     end
+  end
 
-    describe "#reverse" do
-        it "reverse the input string" do
-            expect(@solver.reverse('keenan')).to eq 'naneek'
-        end
+  describe '#reverse' do
+    it 'reverse the input string' do
+      expect(@solver.reverse('keenan')).to eq 'naneek'
     end
+  end
+
+  describe '#fizzbuzz' do
+    it 'returns constructed string' do
+      expect(@solver.fizzbuzz(15)).to eq '1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzbuzz'
+    end
+  end
 end
